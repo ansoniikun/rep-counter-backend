7c9ea041fb86ed3cb656f340670dacded0ed0421
HA Maleke <hm_utilities@tuta.com>
2025-05-06 13:23:48 +0200

    optimse ports

--- app.py
+++ app.py
@@ -437,5 +437,8 @@ def reset_ready():
     return jsonify({'message': 'Ready timer reset'}), 200
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
